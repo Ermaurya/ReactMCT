7916df894a65059306af42830ee5a17789a69d19
Shubham maurya <49495079+Ermaurya@users.noreply.github.com>
2022-11-23 02:47:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 
 import './App.css';
 import { Main } from './components/Main';
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
-     <Main/>
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
     </div>
   );
 }
